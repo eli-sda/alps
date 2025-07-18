b0dd11083aba74a5899cfba8eb8aef9f848a8cb1
Elena Ivanova <webmaster@sdabg.net>
2025-07-18 12:44:11 +0300

    - fixed AccordionItem heading - fixed Asside layout

--- src/organisms/asides/aside/Aside.tsx
+++ src/organisms/asides/aside/Aside.tsx
@@ -1,13 +1,11 @@
-import React from "react";
+import React from 'react';
 
 export interface AsideProps {
-    children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
 export const Aside = ({children}: AsideProps): JSX.Element => {
-    return (
-        <div className={"u-padding--right u-spacing"}>
-            {children}
-        </div>
-    )
-}
+  return (
+    <div className={'u-padding--right u-spacing u-clear-fix'}>{children}</div>
+  );
+};
