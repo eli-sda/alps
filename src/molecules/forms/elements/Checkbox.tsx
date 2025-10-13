56e51361968c6a92ce483d95d556fe210eee29c5
gabyortova <gabi.ortova@gmail.com>
2025-10-13 17:17:03 +0300

    Add onChange property in Checkbox.tsx

--- src/molecules/forms/elements/Checkbox.tsx
+++ src/molecules/forms/elements/Checkbox.tsx
@@ -1,40 +1,43 @@
-import React from "react";
+import React from 'react';
 
-import {FormLabel} from "./FormLabel";
-import {BaseInput} from "./BaseInput";
+import {FormLabel} from './FormLabel';
+import {BaseInput} from './BaseInput';
 
 export interface CheckboxProps {
-    checked?: boolean,
-    error?: string,
-    id?: string,
-    label: string,
-    labelOptional?: string,
-    labelClass?: string,
-    labelSpacing?: string,
-    name: string,
-    placeholder?: string,
-    value?: string,
+  checked?: boolean;
+  error?: string;
+  id?: string;
+  label: string;
+  labelOptional?: string;
+  labelClass?: string;
+  labelSpacing?: string;
+  name: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
 }
 
 export const Checkbox = ({
-                             checked,
-                             label,
-                             labelOptional,
-                             labelClass,
-                             labelSpacing,
-                             ...props
-                         }: CheckboxProps): JSX.Element => {
-    return (
-        <FormLabel
-            className={labelClass}
-            error={props.error}
-            htmlFor={props.id || props.name}
-            position="bottom"
-            text={label}
-            textOptional={labelOptional}
-            // spacing={labelSpacing}
-        >
-            <BaseInput checked={checked} type="checkbox" {...props} />
-        </FormLabel>
-    )
-}
+  checked,
+  label,
+  labelOptional,
+  labelClass,
+  labelSpacing,
+  ...props
+}: CheckboxProps): JSX.Element => {
+  return (
+    <FormLabel
+      className={labelClass}
+      error={props.error}
+      htmlFor={props.id || props.name}
+      position="bottom"
+      text={label}
+      textOptional={labelOptional}
+      // spacing={labelSpacing}
+    >
+      <BaseInput checked={checked} type="checkbox" {...props} />
+    </FormLabel>
+  );
+};
