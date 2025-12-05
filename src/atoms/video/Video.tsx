b2e9fef2b1a8517deb9a25beb7f1001b453887af
gabyortova <gabi.ortova@gmail.com>
2025-12-05 14:22:35 +0200

    Fix Video component errors

--- src/atoms/video/Video.tsx
+++ src/atoms/video/Video.tsx
@@ -1,37 +1,27 @@
 import React from 'react';
-const wrapStyle = {
-    paddingTop: `${(100 / 16) * 9}%`, // enforces 16:9 aspect ratio. TODO: Make this calculated based on video's original aspect ratio?
-}
-
-const iframeStyle = {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    top: 0,
-    left: 0,
-}
 
 export interface VideoProps {
-    src: string,
-    width?: number,
-    height?: number
+  src: string;
+  width?: number;
+  height?: number;
+  title?: string;
 }
 
-export const Video = ({ src, width, height}: VideoProps): JSX.Element => {
-    return (
-        <div style={wrapStyle}>
-            <iframe
-                src={src}
-                width={width}
-                height={height}
-                frameBorder="0"
-                webkitallowfullscreen="true"
-                mozallowfullscreen="true"
-                allowFullScreen
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                style={iframeStyle}
-            />
-        </div>
-    )
-}
+export const Video = ({
+  src,
+  width,
+  height,
+  title = 'title'
+}: VideoProps): JSX.Element => {
+  return (
+    <div>
+      <iframe
+        title={title}
+        src={src}
+        width={width}
+        height={height}
+        allowFullScreen
+      />
+    </div>
+  );
+};
