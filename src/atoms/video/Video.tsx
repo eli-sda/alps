5b66331ddabb85838c5d68ad8520242d2229e5af
Elena Ivanova <webmaster@sdabg.net>
2025-12-08 10:32:04 +0200

    Video - set fixed title, aspect ratio to iframe and autoplay if needed

--- src/atoms/video/Video.tsx
+++ src/atoms/video/Video.tsx
@@ -10,17 +10,20 @@ export interface VideoProps {
 export const Video = ({
   src,
   width,
-  height,
-  title = 'title'
+  height
 }: VideoProps): JSX.Element => {
+  // Check if src has autoplay=1 query param
+  const shouldAutoplay = src.includes('autoplay=1');
   return (
     <div>
       <iframe
-        title={title}
+        style={{aspectRatio: '16 / 9'}}
+        title="YouTube video player"
         src={src}
         width={width}
         height={height}
         allowFullScreen
+        allow={shouldAutoplay ? 'autoplay' : undefined}
       />
     </div>
   );
--- src/molecules/media/figure/Figure.tsx
+++ src/molecules/media/figure/Figure.tsx
@@ -1,69 +1,73 @@
-import React, {useMemo} from "react"
-import {Video} from "../../../atoms/video/Video";
-import {Figcaption} from "../figcaption/Figcaption";
-import {Picture} from "../../../atoms/images/Picture";
-import {figureAlignPositionsMap, figureSizesMap, getFigureClasses} from "../../../global/figures";
-import {ImageType} from "../../../atoms/images/ImageType";
+import React, {useMemo} from 'react';
+import {Video} from '../../../atoms/video/Video';
+import {Figcaption} from '../figcaption/Figcaption';
+import {Picture} from '../../../atoms/images/Picture';
+import {
+  figureAlignPositionsMap,
+  figureSizesMap,
+  getFigureClasses
+} from '../../../global/figures';
+import {ImageType} from '../../../atoms/images/ImageType';
 
 export interface FigureProps {
-    /**
-     * Specify the caption of your  Figure
-     */
-    caption?: string,
-    /**
-     * Specify the align of your  Figure
-     */
-    align?: keyof typeof figureAlignPositionsMap,
-    /**
-     * Specify the className of your  Figure
-     */
-    className?: string,
-    image?: ImageType,
-    /**
-     * Specify whether the Figure should be an lazy variant
-     */
-    lazy?: boolean,
-    /**
-     * Specify the size of your  Figure
-     */
-    size?: keyof typeof figureSizesMap,
-    /**
-     * Specify the videoSrc of your  Figure
-     */
-    videoSrc?: string
+  /**
+   * Specify the caption of your  Figure
+   */
+  caption?: string;
+  /**
+   * Specify the align of your  Figure
+   */
+  align?: keyof typeof figureAlignPositionsMap;
+  /**
+   * Specify the className of your  Figure
+   */
+  className?: string;
+  image?: ImageType;
+  /**
+   * Specify whether the Figure should be an lazy variant
+   */
+  lazy?: boolean;
+  /**
+   * Specify the size of your  Figure
+   */
+  size?: keyof typeof figureSizesMap;
+  /**
+   * Specify the videoSrc of your  Figure
+   */
+  videoSrc?: string;
 }
 
 export const Figure = ({
-                           caption,
-                           className = "",
-                           image,
-                           lazy = false,
-                           videoSrc,
-                           align,
-                           size
-                       }: FigureProps): JSX.Element => {
-    let classes = "";
-    if (align && size) {
-        classes = useMemo(() => getFigureClasses(align, size), [align, size])
-    }
+  caption,
+  className = '',
+  image,
+  lazy = false,
+  videoSrc,
+  align,
+  size
+}: FigureProps): JSX.Element => {
+  const classes = useMemo(
+    () => (align && size ? getFigureClasses(align, size) : ''),
+    [align, size]
+  );
 
-    return (
-        <figure className={`o-figure ${classes} ${className}`}>
-            {image && (
-                <div className="o-figure__image">
-                    <Picture image={image} lazy={lazy}/>
-                </div>
-            )}
-            {videoSrc && (
-                <div className="u-image--wrap">
-                    <Video src={videoSrc}/>
-                </div>
-            )}
-            {caption && (
-                <div className="o-figure__caption">
-                    <Figcaption>{caption}</Figcaption>
-                </div>
-            )}
-        </figure>
-    )
-}
+  return (
+    <figure className={`o-figure ${classes} ${className}`}>
+      {image && (
+        <div className="o-figure__image">
+          <Picture image={image} lazy={lazy} />
+        </div>
+      )}
+      {videoSrc && (
+        <div className="u-image--wrap">
+          <Video src={videoSrc} />
+        </div>
+      )}
+      {caption && (
+        <div className="o-figure__caption">
+          <Figcaption>{caption}</Figcaption>
+        </div>
+      )}
+    </figure>
+  );
+};
