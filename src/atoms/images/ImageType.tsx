eb67a1b78d9a4db5aed0fca174c835b34f835fc8
Elena Ivanova <webmaster@sdabg.net>
2026-01-18 18:00:04 +0200

    Deleted obsolete constant DefaultImage

--- src/atoms/images/ImageType.tsx
+++ src/atoms/images/ImageType.tsx
@@ -1,17 +1,8 @@
-import {SourceSet} from "./SourceSet";
+import {SourceSet} from './SourceSet';
 
 export interface ImageType {
-    alt: string,
-    srcSet: SourceSet,
-    caption?: string,
-    portrait?: boolean
-}
-
-export const DefaultImage = {
-    srcSet: {
-        default: "",
-        500: "",
-        750: "",
-        1200: ""
-    }, alt: ''
+  alt: string;
+  srcSet: SourceSet;
+  caption?: string;
+  portrait?: boolean;
 }
